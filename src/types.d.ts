3faad6e080ce2815da70912029d8724e382969cf
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-07 00:39:59 +0100

    added type definitions

---
+++ src/types.d.ts
@@ -0,0 +1,47 @@
+import exp from "constants"
+
+export type connectPhotosData = {
+	id: number,
+}
+
+export type CreatePhotoData = {
+	title: string,
+	url: string,
+	comment?: string,
+	user_id: number,
+}
+
+export type CreateAlbumData = {
+	title: string,
+	user_id: number,
+}
+
+export type CreateUserData = {
+	first_name: string,
+	last_name: string,
+	email: string,
+	password: string,
+}
+
+export type JwtPayload = {
+	sub: number,
+	email: string,
+	iat?: number,
+	exp?: number,
+}
+
+export type UpdateAlbumData = {
+	title?: string,
+}
+
+export type UpdatePhotoData = {
+	title?: string,
+	url?: string,
+	comment?: string,
+}
+
+export type UpdateUserData = {
+	name?: string,
+	email?: string,
+	password?: string,
+}
