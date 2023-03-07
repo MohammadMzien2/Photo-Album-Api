44b7dc8c41c39985d4c459622beda5908648a7c3
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-07 10:37:16 +0100

    photo albums router

---
+++ src/routes/albums.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { connectPhotosRules, createAlbumRules, updateAlbumRules } from '../validations/album_rules'
+import { index, show, store, update } from '../controllers/album_controller'
+const router = express.Router()
+
+/**
+ * Get all albums
+ * GET/ albums
+ */
+router.get('/', index)
+
+/**
+ * GET a single album
+ * GET/ albums/:albumId
+ */
+router.get('/:albumId', show)
+
+/**
+ * Create a new album
+ * POST/albums
+ */
+router.post('/', createAlbumRules, store)
+
+/**
+ * PATCH/ albums/:albumId
+ * Update an album
+ */
+router.patch('/:albumId', updateAlbumRules, update)
+
+export default router
