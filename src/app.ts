cfca606b7160a6750519f3b478218027f97b111c
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-07 10:36:11 +0100

    import and use

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-import express from "express"
-import morgan from "morgan"
+import express from "express";
+import morgan from "morgan";
 import routes from './routes'
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
-
-// Use routes
 app.use(routes)
 
 export default app
