c7504ffe666f230b17650768794bf78a4ecd6c5a
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-07 10:37:44 +0100

    get and create a user

---
+++ src/services/user_service.ts
@@ -0,0 +1,48 @@
+import prisma from "../prisma";
+import { CreatePhotoData, CreateUserData, UpdatePhotoData, UpdateUserData } from '../types'
+
+/**
+ * Get a user by id
+ */
+
+export const getUserById = async (id: number) => {
+	return await prisma.user.findUnique({
+		where: {
+			id: id
+		}
+	})
+}
+
+/**
+ * Get a user by email
+ */
+export const getUserByEmail = async (email: string) => {
+	return await prisma.user.findUnique({
+		where: {
+			email: email,
+		}
+	})
+}
+
+/**
+ * Create a user
+ */
+
+export const createUser = async (data: CreateUserData) => {
+	return await prisma.user.create({
+		data: data,
+	})
+}
+
+/**
+ * Update a user
+ */
+
+export const updateUser = async (userId: number, userData: UpdateUserData) => {
+	return await prisma.user.update({
+		where: {
+			id: userId,
+		},
+		data: userData,
+	})
+}
