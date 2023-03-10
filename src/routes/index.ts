48ed302a79a62735c8a51fadf8276782b5d04d80
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-10 15:10:45 +0100

    added .env.example

---
+++ src/routes/index.ts
@@ -0,0 +1,46 @@
+import albums from './albums'
+import photos from './photos'
+import express from 'express'
+import { validateToken } from '../middlewares/auth/jwt'
+import { login, refresh, register } from '../controllers/user_controller'
+import { createUserRules, loginUserRules } from '../validations/user_rules'
+
+//Instantiate a new router
+const router = express.Router()
+
+/**
+ * GET /
+ */
+router.get('/', (req, res) => {
+	res.send({
+		message: "I AM API, BEEP BOOP",
+	})
+})
+
+/**
+ * albums of photos
+ */
+router.use('/albums', validateToken, albums)
+
+/**
+ * photos from user
+ */
+router.use('/photos', validateToken, photos)
+
+/**
+ * POST/ login to profile
+ */
+router.post('/login', loginUserRules, login)
+
+/**
+ * POST/ refresh the profile login
+ */
+router.post('/refresh', refresh)
+
+/**
+ * POST /register a profile
+ */
+
+router.post('register', createUserRules, register)
+
+export default router
